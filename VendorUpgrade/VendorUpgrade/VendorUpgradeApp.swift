eaa6710426eef8bda4eb341c1ab0744ee86e6f89
diosanshu <diosanshu@gmail.com>
2023-12-23 07:33:13 +0530

    Added Xcode IDE

---
+++ VendorUpgrade/VendorUpgrade/VendorUpgradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorUpgradeApp.swift
+//  VendorUpgrade
+//
+//  Created by Haadhya on 23/12/23.
+//
+
+import SwiftUI
+
+@main
+struct VendorUpgradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
